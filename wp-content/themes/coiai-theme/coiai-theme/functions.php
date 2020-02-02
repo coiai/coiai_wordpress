1ece551d67066460833a56fd2f8f8f2e51e057aa
coiai <coiaichan@gmail.com>
2020-02-03 04:29:08 +0900

    fix:removed unnesessary files
    元々あった不要なファイルの為のコードを削除しました

--- wp-content/themes/coiai-theme/coiai-theme/functions.php
+++ wp-content/themes/coiai-theme/coiai-theme/functions.php
@@ -122,40 +122,10 @@ add_action( 'widgets_init', 'coiai_theme_widgets_init' );
 function coiai_theme_scripts() {
 	wp_enqueue_style( 'coiai-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'coiai-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// wp_enqueue_script( 'coiai-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'coiai-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	// 	wp_enqueue_script( 'comment-reply' );
+	// }
 }
 add_action( 'wp_enqueue_scripts', 'coiai_theme_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
