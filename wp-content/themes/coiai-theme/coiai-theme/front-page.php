989f0442724d08995d66741efef616d767c29475
coiai <coiaichan@gmail.com>
2020-02-03 16:03:25 +0900

    add:made post roop
    投稿の取得をするためです。

--- wp-content/themes/coiai-theme/coiai-theme/front-page.php
+++ wp-content/themes/coiai-theme/coiai-theme/front-page.php
@@ -12,5 +12,18 @@
         <h2 class="sec2title">NEWS</h2>
     </div>
 
+    <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+    ?>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+            <?php
+            endwhile;
+        else :
+            echo wpautop( '投稿が見つかりませんでした。');
+        endif;
+    ?>
+
 <?php
 get_footer();
